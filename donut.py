736df2d5dcea9eb8e46f67cca2be072c0b38b8ba
Fahmi Sabila Dinnulhaq <fahmisabiladinnulhaq@gmail.com>
2023-04-27 10:48:39 +0700

    change

--- donut.py
+++ donut.py
@@ -25,7 +25,7 @@ y_offset = rows / 2
 A, B = 0, 0  # rotating animation
 
 theta_spacing = 10
-phi_spacing = 1 # for faster rotation change to 2, 3 or more, but first change 86, 87 lines as commented
+phi_spacing = 2 # for faster rotation change to 2, 3 or more, but first change 86, 87 lines as commented
 
 chars = ".,-~:;=!*#$@"  # luminance index
 
@@ -78,8 +78,8 @@ while run:
         y_start = 0
 
     for i in range(len(b)):
-        A += 0.00006  # for faster rotation change to bigger value
-        B += 0.00003  # for faster rotation change to bigger value
+        A += 0.00004  # for faster rotation change to bigger value
+        B += 0.00002  # for faster rotation change to bigger value
         if i == 0 or i % columns:
             text_display(b[i], x_start, y_start)
             x_start += x_separator
